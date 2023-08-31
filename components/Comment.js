b439104ac9805734803ff2ee33f4d75dd1f399f7
mpassava <mpassava@gmail.com>
2023-08-31 19:25:54 -0400

    display item page and comments

--- components/Comment.js
+++ components/Comment.js
@@ -9,6 +9,3 @@ export default function Comment(comment) {
         </div>
     `
 }
-
-
-// I&#x27;ve been sort-of adjacent to a lot of these groups when they were in their infancy (hackr.org darkdevelopments.com ssgroup.org etc;) and it&#x27;s really cool how the majority of the kids who were running those sites in the 2000&#x27;s are now so keen to create <i>better</i> environments for others to learn.<p>Hackthissite is another excellent resource from the same pedigree of people
